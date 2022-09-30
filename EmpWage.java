7d404fd7abef8ad264bc7a862864acd5e9794b04
vishakhakshirsagar <kshirsagarvishakha22@gmail.com>
2022-10-01 00:00:22 +0530

    Added UC8: Wage calculation for multiple companies

--- EmpWage.java
+++ EmpWage.java
@@ -1,58 +1,67 @@
 package com.EmployeeWage;
-import java.util.Scanner;
+
 import java.util.Random;
 public class EmpWage {
-    Random r = new Random();//UC1
-    int wagePerHour=20;
-    int empHour;
+    public static final int IS_FULL_TIME = 2, IS_PART_TIME = 1;
+    public  int EMP_RATE_PER_HOUR, fullTimeHrs, partTimeHrs ;
+    public  int MAX_HRS_IN_MONTHS, totalEmpHrs, numOfWorkingDays;
+    String companyName;
 
-    void EmployeeWage(int workDays) {
-        int empcheck = r.nextInt(3);
-        // UC4 Switch case used to check employee attendance status
-        switch(empcheck){ // UC1 to check the Employee is present or absent using RANDOM class.
-            case 0:
-                empHour=8;
-                System.out.println("Employee is Present for "+empHour+" hours. ");
-                break;
-            case 1:// UC3 to add employee hours present for part time
-                empHour=4;
-                System.out.println("Employee present for part time for "+empHour+" hours. ");
-                break;
+    EmpWage(String companyName, int EMP_RATE_PER_HOUR, int MAX_HRS_IN_MONTHS,
+                 int numOfWorkingDays, int fullTimeHrs, int partTimeHrs ){
 
-            default:
-                empHour=0;
-                System.out.println("Employee is Absent so no salary for him...");
-        }
-        int wage = wagePerHour*empHour; // UC2 to calculate the daily wage for present employee
-        System.out.println("Employee Wage for one day is: "+wage);
-        //uc5 to calculate monthly wage of employee
-        System.out.println("Enter number of working days in one month: ");
-
-        int monthlyWage = wage*workDays;
-        int monthlyHour = empHour*workDays;
-        System.out.println("Employee monthly salary will be : "+monthlyWage);
-
-        //uc6 to check if the Employee working hours or days meets the company requirements
-        if (workDays >=20 || monthlyHour >=100){
-            System.out.println("Worker work for 20 days or 100 hrs.");
-            System.out.println("The worker monthly Salary is: "+monthlyWage);
-        }
-        else{
-            System.out.println("Worker did not work for 20 days or 100 hrs.");
-            System.out.println("no salary for worker.");
-        }
+        this.companyName = companyName;
+        this.EMP_RATE_PER_HOUR = EMP_RATE_PER_HOUR;
+        this.MAX_HRS_IN_MONTHS = MAX_HRS_IN_MONTHS;
+        this.numOfWorkingDays = numOfWorkingDays;
+        this.partTimeHrs = partTimeHrs;
+        this.fullTimeHrs = fullTimeHrs;
 
     }
+
+    void companyInfo(){
+        System.out.println("Company Name\t" + companyName + " Employee rate per hr\t" + EMP_RATE_PER_HOUR +
+                " Max hrs in a month\t" + MAX_HRS_IN_MONTHS + " No. of working days\t" + numOfWorkingDays +
+                " Part time working hr\t" + partTimeHrs + " Full time working hr\t" + fullTimeHrs);
+    }
+
+
+    public void wageComputation() {
+        int dayCount = 0, empwage = 0;
+        int temp=0;
+
+        Random random = new Random();
+        while ( totalEmpHrs <= MAX_HRS_IN_MONTHS && dayCount < numOfWorkingDays ) {
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
+                case IS_FULL_TIME:
+                    empwage = fullTimeHrs * EMP_RATE_PER_HOUR * numOfWorkingDays;
+                    dayCount++;
+                    totalEmpHrs+=fullTimeHrs;
+                    empwage += empwage;
+                    break;
+                case IS_PART_TIME:
+                    empwage = partTimeHrs * EMP_RATE_PER_HOUR * numOfWorkingDays;
+                    dayCount++;
+                    totalEmpHrs+=partTimeHrs;
+                    empwage += empwage;
+                    break;
+                default:
+            }
+        }
+        System.out.println("Daycount\t"+ dayCount + " "+ "Total employee hours" + totalEmpHrs);
+        System.out.println("Employee Wage : " + empwage);
+    }
+
+    // Below is the main method EmpWageComputation program.
+    //@param args
     public static void main(String[] args) {
-        System.out.println("Welcome To Employee Wage Computation");
-        EmpWage obj = new EmpWage();
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Employee Working Days");
-        int n = sc.nextInt();
-        obj.EmployeeWage(n);
-
-
-
+        EmpWage dell = new EmpWage("Infosys",500,160,21,6,3);
+        dell.companyInfo();
+        dell.wageComputation();
+        EmpWage lenovo = new EmpWage("Deloitte",550,150,22,8,4);
+        lenovo.companyInfo();
+        lenovo.wageComputation();
     }
-}
 
+}
