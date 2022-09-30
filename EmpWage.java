9034cc9c445876c3ee27ab70e2f19c6a9f23e2b8
vishakhakshirsagar <kshirsagarvishakha22@gmail.com>
2022-09-30 18:43:56 +0530

    Added UC1: Employee Attendance Check

--- EmpWage.java
+++ EmpWage.java
@@ -1,8 +1,15 @@
 package com.EmployeeWage;
-
+import java.util.Random;
 public class EmpWage {
     public static void main(String[] args)
     {
         System.out.println("Welcome To Employee Wage Computation");
+        Random r = new Random();
+        int empcheck = r.nextInt(2) + 1;
+        if (empcheck == 1) {
+            System.out.println("Employee is present");
+        } else {
+            System.out.println("Employee Is Absent");
+        }
     }
 }
